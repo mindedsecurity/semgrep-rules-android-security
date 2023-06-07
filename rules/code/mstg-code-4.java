e5c6d2d7779157462d0128102bfa9925d83aae70
gand3lf <riccardo.cardelli1994@gmail.com>
2023-06-07 15:32:19 +0200

    mstg-code-4

---
+++ rules/code/mstg-code-4.java
@@ -0,0 +1,17 @@
+public class A{
+    public void onCreate() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
+        super.onCreate();
+    }
+}
