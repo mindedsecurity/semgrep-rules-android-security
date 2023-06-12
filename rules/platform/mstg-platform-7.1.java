e85c324f7bea77f1b46eca4ad1e5e40e2945d83e
gand3lf <riccardo.cardelli1994@gmail.com>
2023-06-12 22:19:21 +0200

    header fix

--- rules/platform/mstg-platform-7.1.java
+++ rules/platform/mstg-platform-7.1.java
@@ -10,9 +10,4 @@ public class WebAppInterface {
     public void showToast2(String toast) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
     }
-    public void test4(){
-        MSTG_ENV_008_JS_Interface jsInterface = new MSTG_ENV_008_JS_Interface(this);
-
-        myWebView.addJavascriptInterface(jsInterface, "Android");
-    }
 }
