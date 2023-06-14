58f4b0587fba7ab2e467beb9e325503a7e77508e
gand3lf <riccardo.cardelli1994@gmail.com>
2023-06-14 23:31:19 +0200

    Update mstg-crypto-6

--- rules/crypto/mstg-crypto-6.java
+++ rules/crypto/mstg-crypto-6.java
@@ -2,16 +2,15 @@ import java.util.Random;
 import java.lang.*;
 import java.security.SecureRandom;
 
-
 public class A{
     String a = "test";
     private int test(){
         return 1 + new Random().nextInt(6);
     }
-    private int test2(){
+    private int test2random(){
         return 1 + Math.random();
     }
-    private int test3(){
+    private int test3gen(){
         Random r = new Random();
         return r.nextDouble();
     }
@@ -19,6 +18,7 @@ public class A{
         SecureRandom number = new SecureRandom(12);
         return number.nextInt(21);
     }
+    //This case is not cover
     private int random(){
         int b = 3;
         return b + 10;
@@ -26,5 +26,12 @@ public class A{
     private int random(){
         SecureRandom number = new SecureRandom();
         return number.nextInt(21);
+    }
+    private int gen_token(){
+        return abc(new Date().getTime());
+    }
+    private int gen_token(){
+        Calendar c = Calendar.getInstance(); 
+        int mseconds = c.get(Calendar.MILLISECOND)
     }
 }
