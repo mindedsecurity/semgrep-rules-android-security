d95a44a35cf734f9b4722ec1c310c2cbb7d74843
federicodotta <dottafederico@gmail.com>
2023-07-12 09:35:15 +0200

    7.2 and 7.3 merged

--- rules/platform/mstg-platform-7.2.java
+++ rules/platform/mstg-platform-7.2.java
@@ -1,8 +1,17 @@
 public class WebAppInterface {
     Context mContext;
-    public void test4(){
+    public void test1(){
         MSTG_ENV_008_JS_Interface jsInterface = new MSTG_ENV_008_JS_Interface(this);
 
         myWebView.addJavascriptInterface(jsInterface, "Android");
+    }
+}
+public class WebAppInterface extends WebView {
+    Context mContext;
+    public void test2(){
+        addJavascriptInterface(new MSTG_ENV_008_JS_Interface(this), "Android");
+    }
+    public void test3(){
+        this.addJavascriptInterface(new MSTG_ENV_008_JS_Interface(this), "Android");
     }
 }
--- rules/platform/mstg-platform-7.3.java
+++
@@ -1,6 +0,0 @@
-public class WebAppInterface extends WebView {
-    Context mContext;
-    public void test4(){
-        addJavascriptInterface(new MSTG_ENV_008_JS_Interface(this), "Android");
-    }
-}
