6df77251b5b751b0823298fa5a66cbac0cacd0cb
gand3lf <riccardo.cardelli1994@gmail.com>
2023-09-18 16:21:18 +0200

    update mstg-storage-9

--- rules/storage/mstg-storage-9.java
+++ rules/storage/mstg-storage-9.java
@@ -1,16 +1,23 @@
 public class MainActivity extends AppCompatActivity {
-    private void test(){
-        // ok: MSTG-STORAGE-9
-        getWindow().setFlags(8192,
-                8192);
-    }
-    private void test2(){
-        Window window = activity.getWindow();
-        // ok: MSTG-STORAGE-9
-        window.setFlags(8192,
-                8192);
-        // ruleid: MSTG-STORAGE-9
-        window.setFlags(1024,
-                1024);
-    }
+     private void test(){
+          getWindow().setFlags(8192, 8192);
+     }
+     private void test2(){
+          Window window = activity.getWindow();
+          window.setFlags(8192, 8192);
+          // ruleid: MSTG-STORAGE-9
+          window.setFlags(1024, 1024);
+     }
+}
+// ruleid: MSTG-STORAGE-9
+public class MainActivity2 extends AppCompatActivity {
+     private void test(){
+          //getWindow().addFlags(8192);
+     }
+}
+// ok: MSTG-STORAGE-9
+public class MainActivity3 extends AppCompatActivity {
+     private void test(){
+          getWindow().addFlags(8192);
+     }
 }
